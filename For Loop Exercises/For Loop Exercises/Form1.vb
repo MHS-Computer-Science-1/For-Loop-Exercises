f0db416631191c22c6b8195cf6863dfd76503b63
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-09-14 16:13:40 -0400

    Add files via upload

---
+++ For Loop Exercises/For Loop Exercises/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        'Clear the ListBox
+        ListBox1.Items.Clear()
+    End Sub
+
+
+
+End Class
