d03cb85f0bd495fb4985dc6e20b2c162adeaa4d0
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-09-14 16:12:53 -0400

    Delete For Loop Exercises directory

--- For Loop Exercises/For Loop Exercises/Form1.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form1
-    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
-        'Clear the ListBox
-        ListBox1.Items.Clear()
-    End Sub
-End Class
